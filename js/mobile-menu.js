db12ec3d500e7dffa86917a2270cc3f67041c9bd
Alfat1989 <gilmalf89@gmail.com>
2021-12-12 04:23:28 +0500

    Добавляет мобильное меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,3 +1,10 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
@@ -13,7 +20,6 @@
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 600px)').addEventListener('change', e => {
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
